aecd540bb8905c056713b67d0d58e1280304fee5
Gustavo Uliano <gustavouliano11@gmail.com>
2024-09-30 22:23:58 -0300

    changed for frontend and api folders

---
+++ api/src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require '../vendor/autoload.php';
+require_once './routes.php';
--- src/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Doctrine\DBAL\DriverManager;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
-
-require '../vendor/autoload.php';
-require_once './routes.php';
